761765c055f222504831a166954786544fab1d5e
ZurielAbasola <zuriel123.abasola@gmail.com>
2024-02-23 21:05:14 -0700

    format

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className='lg:col-span-7 place-self-center'>
-                <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
-                    Hello, I'm Zuriel
-                </h1>
-                <p className='text-[#ADB7BE] text-lg lg:text-lg'>
-                    I'm a full-stack developer with a 
-                    passion for building beautiful and 
-                    functional web applications.
-                </p>
-            </div>
-            <div className='lg:col-span-5'>
-                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-                    <Image
-                        src='/images/image.jpg'
-                        alt='Hero Image'
-                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                        width={300}
-                        height={300}
-                    />
-                </div>
-            </div>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="lg:col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I'm Zuriel
+          </h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-lg">
+            I'm a full-stack developer with a passion for building beautiful and
+            functional web applications.
+          </p>
         </div>
+        <div className="lg:col-span-5">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/image.jpg"
+              alt="Hero Image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
