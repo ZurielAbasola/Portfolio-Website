fb9c1575ba2c83622cae1661d34dbcbc28c5dd8f
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-02-24 23:20:46 -0700

    Buttons

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,19 +5,28 @@ const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="lg:col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'm Zuriel
+        <div className="lg:col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-900 via-blue-600 to-purple-600 mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-black">Hello, I'm </span>
+            Zuriel
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-lg">
+          <p className="text-[#272727] text-lg sm:text-lg mb-6 lg:text-xl">
             I'm a full-stack developer with a passion for building beautiful and
             functional web applications.
           </p>
+          <div>
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit hover:bg-gradient-to-tr hover:from-blue-900 hover:via-blue-600 hover:to-purple-600 mr-4 bg-black text-white font-bold border-transparent">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit bg-transparent hover:bg-slate-300 text-black border border-black mt-3 hover:bg-gradient-to-tr hover:from-blue-900 hover:via-blue-600 hover:to-purple-600 hover:border-transparent hover:text-white">
+              Download Resume
+            </button>
+          </div>
         </div>
-        <div className="lg:col-span-5">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+        <div className="col-span-5 place-self-center mt-4">
+          <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[450px] lg:h-[450px] relative overflow-hidden">
             <Image
-              src="/images/image.jpg"
+              src="/images/image.png"
               alt="Hero Image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
