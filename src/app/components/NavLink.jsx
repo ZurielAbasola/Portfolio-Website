34cffb17c3b30ade5b9586e7b505612209f53a14
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-03-13 22:32:17 -0600

    Navbar

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -6,7 +6,6 @@ const NavLink = ({ href, title }) => {
       href={href}
       className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-blue-700"
     >
-      <br />
       {title}
     </Link>
   );
