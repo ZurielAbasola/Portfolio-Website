3ae8a00d8aa859d3d64c47faf215ae9c59731801
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-06-04 01:08:35 -0600

    Email Section component

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,23 @@
+import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Zuriel <zuriel123.abasola@gmail.com>",
+      to: ["zuriel123.abasola@gmail.com"],
+      subject: "Hello world",
+      react: ,
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
---
+++ src/app/components/EmailTemplate.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+
+interface EmailTemplateProps {
+    firstName
